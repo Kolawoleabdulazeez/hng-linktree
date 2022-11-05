9a1a7e70ce93ef1de126fbd4dbd4b5db8bb17361
Kolawoleabdulazeez <Kolawoleabdulazeez5@gmail.com>
2022-11-06 06:14:53 +0800

    docs: assest folder updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Homepage from './Components/Homepage'
+import Contact from './Pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-    <h1> We go again</h1>
+    <div className='index_page'>
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route exact path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
